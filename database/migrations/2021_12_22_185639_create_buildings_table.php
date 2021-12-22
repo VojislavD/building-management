e8db2c7dc9100a40c64cfdff29f81ee6566b3957
VojislavD <vojislavdragicevic17@gmail.com>
2021-12-22 22:03:11 +0100

    Create database fields for building

--- database/migrations/2021_12_22_185639_create_buildings_table.php
+++ database/migrations/2021_12_22_185639_create_buildings_table.php
@@ -15,6 +15,24 @@ class CreateBuildingsTable extends Migration
     {
         Schema::create('buildings', function (Blueprint $table) {
             $table->id();
+            $table->integer('internal_code');
+            $table->string('pib');
+            $table->string('identification_number');
+            $table->string('account_number');
+            $table->string('balance');
+            $table->string('construction_year');
+            $table->boolean('yard');
+            $table->boolean('elevator');
+            $table->string('address');
+            $table->string('city');
+            $table->string('municipality');
+            $table->string('postal_code');
+            $table->tinyInteger('floors');
+            $table->tinyInteger('apartments');
+            $table->tinyInteger('tenants');
+            $table->tinyInteger('square');
+            $table->string('balance_begining');
+            $table->text('comment');
             $table->timestamps();
         });
     }
