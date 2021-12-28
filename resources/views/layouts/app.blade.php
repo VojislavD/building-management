ba5c57a8eae0dfe5fe921a8e0b5a318e2a901d6d
VojislavD <vojislavdragicevic17@gmail.com>
2021-12-28 19:17:16 +0100

    Change color of profile icon in top menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,7 +151,7 @@
                                             @mouseleave="linkHover = false"
                                         >
                                             <div class="flex items-center">
-                                                <svg class="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+                                                <svg class="w-5 h-5 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                                                 <div class="text-sm ml-3">
                                                     <p 
                                                         class="text-gray-600 font-bold capitalize"
