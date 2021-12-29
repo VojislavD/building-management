e94720583bfa8b5f0241ae58f875bf3741c887fb
VojislavD <vojislavdragicevic17@gmail.com>
2021-12-29 12:28:47 +0100

    Add edit button to show building page

--- resources/views/buildings/show.blade.php
+++ resources/views/buildings/show.blade.php
@@ -1,23 +1,35 @@
 <x-app-layout>
-    <div>
-        <div class="flex items-center space-x-4">
-            <span class="text-2xl font-bold" title="{{ __('Address') }}">{{ $building->address }}</span>
-            {!! $building->getStatusLabel() !!}
-            <div class="flex items-center mt-0.5 space-x-1" title="{{ __('Balance') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-600" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                <span class="font-bold">{{ $building->balance }}</span>
+    <div class="flex items-start justify-between">
+        <div>
+            <div class="flex items-center space-x-4">
+                <span class="text-2xl font-bold" title="{{ __('Address') }}">{{ $building->address }}</span>
+                {!! $building->getStatusLabel() !!}
+                <div class="flex items-center mt-0.5 space-x-1" title="{{ __('Balance') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-600" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                    <span class="font-bold">{{ $building->balance }}</span>
+                </div>
+            </div>
+            <p title="{{ __('City') }}">{{ $building->city }}</p>
+            <div class="flex items-center mt-0.5 space-x-6">
+                <div class="flex items-center space-x-1" title="{{ __('Apartments') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-secondary" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                    <span class="text-sm">{{ $building->apartments }}</span>
+                </div>
+                <div class="flex items-center space-x-1" title="{{ __('Tenants') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-secondary" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                    <span class="text-sm">{{ $building->tenants }}</span>
+                </div>
             </div>
         </div>
-        <p title="{{ __('City') }}">{{ $building->city }}</p>
-        <div class="flex items-center mt-0.5 space-x-6">
-            <div class="flex items-center space-x-1" title="{{ __('Apartments') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-secondary" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-                <span class="text-sm">{{ $building->apartments }}</span>
-            </div>
-            <div class="flex items-center space-x-1" title="{{ __('Tenants') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-secondary" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                <span class="text-sm">{{ $building->tenants }}</span>
-            </div>
+        <div>
+            <a 
+                href="{{ route('buildings.edit', $building) }}" 
+                class="flex items-center justify-center space-x-1 hover:text-primary" 
+                title="Edit"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                <span class="text-sm">Edit</span>
+            </a>
         </div>
     </div>
     <div class="grid grid-cols-4 gap-8 mt-12">
