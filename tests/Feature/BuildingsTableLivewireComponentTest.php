f923a36921b5fc0dc66d1d6f7c554bff483fb784
VojislavD <vojislavdragicevic17@gmail.com>
2021-12-29 12:52:57 +0100

    Test show buildings when per page is changed

--- tests/Feature/BuildingsTableLivewireComponentTest.php
+++ tests/Feature/BuildingsTableLivewireComponentTest.php
@@ -72,4 +72,32 @@ class BuildingsTableLivewireComponentTest extends TestCase
             ])
             ->assertHasNoErrors();
     }
+
+    /**
+     * @test
+     */
+    public function test_show_buildings_when_per_page_change()
+    {
+        $building1 = Building::factory()->active()->create([
+            'created_at' => now()->subDay()
+        ]);
+
+        $building2 = Building::factory()->active()->create();
+        Building::factory(8)->active()->create();
+        $building3 = Building::factory()->active()->create();
+
+        Livewire::test('buildings-table')
+            ->assertDontSee($building1->internal_code)
+            ->assertSee($building2->internal_code)
+            ->assertSee($building3->internal_code)
+            ->assertSeeInOrder(['Showing', '1', 'to', '10', 'of', Building::active()->count(), 'results'])
+            ->assertHasNoErrors();
+
+        Livewire::test('buildings-table')
+            ->set('perPage', 15)
+            ->assertSee($building1->internal_code)
+            ->assertSee($building2->internal_code)
+            ->assertSee($building3->internal_code)
+            ->assertHasNoErrors();
+    }
 }
