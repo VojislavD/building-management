269fd2996fa3653b885ef33a09a6035e20baf7bf
VojislavD <vojislavdragicevic17@gmail.com>
2022-01-03 23:08:43 +0100

    Test validation for create apartment component

--- tests/Feature/CreateApartmentLivewireComponentTest.php
+++ tests/Feature/CreateApartmentLivewireComponentTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Apartment;
 use App\Models\Building;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -32,6 +33,100 @@ class CreateApartmentLivewireComponentTest extends TestCase
                 __('Tenants'),
                 __('Save'),
             ])
+            ->assertHasNoErrors();
+    }
+
+    /**
+     * @test
+     */
+    public function test_validation()
+    {
+        $building = Building::factory()->create();
+        $apartment = Apartment::factory()->for($building)->create();
+
+        Livewire::test('create-apartment', [
+                'building' => $building
+            ])
+            ->call('submit')
+            ->assertHasErrors([
+                'owner_name' => 'required',
+                'owner_email' => 'required',
+                'owner_phone' => 'required',
+                'number' => 'required',
+                'tenants' => 'required',
+            ]);
+
+        Livewire::test('create-apartment', [
+                'building' => $building
+            ])
+            ->set('owner_name', 1)
+            ->set('owner_email', 1)
+            ->set('owner_phone', 1)
+            ->set('number', 'Not Integer')
+            ->set('tenants', 'Not Integer')
+            ->call('submit')
+            ->assertHasErrors([
+                'owner_name' => 'string',
+                'owner_email' => 'string',
+                'owner_phone' => 'string',
+                'number' => 'integer',
+                'tenants' => 'integer',
+            ]);
+        
+        Livewire::test('create-apartment', [
+                'building' => $building
+            ])
+            ->set('owner_name', 'More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters.')
+            ->set('owner_email', 'notvalidemail')
+            ->set('owner_phone', 'More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters.')
+            ->set('number', -1)
+            ->set('tenants', -1)
+            ->call('submit')
+            ->assertHasErrors([
+                'owner_name' => 'max',
+                'owner_email' => 'email',
+                'owner_phone' => 'max',
+                'number' => 'min',
+                'tenants' => 'min',
+            ]);
+
+        Livewire::test('create-apartment', [
+                'building' => $building
+            ])
+            ->set('owner_name', 'Test User')
+            ->set('owner_email', 'More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters. More than 255 characters.')
+            ->set('owner_phone', '0641234567')
+            ->set('number', 10000)
+            ->set('tenants', 10000)
+            ->call('submit')
+            ->assertHasErrors([
+                'owner_email' => 'max',
+                'number' => 'max',
+                'tenants' => 'max',
+            ]);
+                
+        Livewire::test('create-apartment', [
+                'building' => $building
+            ])
+            ->set('owner_name', 'Test User')
+            ->set('owner_email', 'testuser@example.com')
+            ->set('owner_phone', '0641234567')
+            ->set('number', $apartment->number)
+            ->set('tenants', 15)
+            ->call('submit')
+            ->assertHasErrors([
+                'number' => 'unique',
+            ]);
+        
+        Livewire::test('create-apartment', [
+                'building' => $building
+            ])
+            ->set('owner_name', 'Test User')
+            ->set('owner_email', 'testuser@example.com')
+            ->set('owner_phone', '0641234567')
+            ->set('number', $apartment->number+1)
+            ->set('tenants', 15)
+            ->call('submit')
             ->assertHasNoErrors();
     }
 }
