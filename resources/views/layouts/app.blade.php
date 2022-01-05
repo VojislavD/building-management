6e8e9ec16d383b218e1f9b44b6798d48611b1af3
VojislavD <vojislavdragicevic17@gmail.com>
2022-01-05 12:41:36 +0100

    Add Tasks to menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,14 +111,14 @@
                         @mouseleave = "linkHover = false"
                         href="{{ route('apartments.index') }}"
                         class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
-                        :class="{{ Request::segment(1) === 'apartments' }} ? 'bg-black bg-opacity-30' : ''"
+                        :class="{{ Request::segment(1) === 'tasks' }} ? 'bg-black bg-opacity-30' : ''"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
                         <span 
                             class="ml-3 transition duration-200" 
                             :class="linkHover ? 'text-gray-100' : ''"
                         >
-                            {{ __('Issues') }}
+                            {{ __('Tasks') }}
                         </span>
                     </a>
                     <!-- end::Menu link -->
