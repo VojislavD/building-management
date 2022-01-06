b25de36f3e2fdd95aeca8e3a23b3629477c02f44
VojislavD <vojislavdragicevic17@gmail.com>
2022-01-06 22:22:28 +0100

    Test tasksTable livewire component when status change

--- tests/Feature/TasksTableLivewireComponentTest.php
+++ tests/Feature/TasksTableLivewireComponentTest.php
@@ -27,4 +27,55 @@ class TasksTableLivewireComponentTest extends TestCase
             ->assertDontSee($cancelled->limited_description)
             ->assertHasNoErrors();
     }
+
+    /**
+     * @test
+     */
+    public function test_show_completed_tasks()
+    {
+        $pending = Task::factory()->pending()->create();
+        $completed = Task::factory()->completed()->create();
+        $cancelled = Task::factory()->cancelled()->create();
+
+        Livewire::test('tasks-table')
+            ->set('status', Task::STATUS_COMPLETED)
+            ->assertSee($completed->limited_description)
+            ->assertDontSee($pending->limited_description)
+            ->assertDontSee($cancelled->limited_description)
+            ->assertHasNoErrors();
+    }
+
+    /**
+     * @test
+     */
+    public function test_show_cancelled_tasks()
+    {
+        $pending = Task::factory()->pending()->create();
+        $completed = Task::factory()->completed()->create();
+        $cancelled = Task::factory()->cancelled()->create();
+
+        Livewire::test('tasks-table')
+            ->set('status', Task::STATUS_CANCELLED)
+            ->assertSee($cancelled->limited_description)
+            ->assertDontSee($completed->limited_description)
+            ->assertDontSee($pending->limited_description)
+            ->assertHasNoErrors();
+    }
+
+    /**
+     * @test
+     */
+    public function test_show_all_tasks()
+    {
+        $pending = Task::factory()->pending()->create();
+        $completed = Task::factory()->completed()->create();
+        $cancelled = Task::factory()->cancelled()->create();
+
+        Livewire::test('tasks-table')
+            ->set('status', '')
+            ->assertSee($pending->limited_description)
+            ->assertSee($completed->limited_description)
+            ->assertSee($cancelled->limited_description)
+            ->assertHasNoErrors();
+    }
 }
