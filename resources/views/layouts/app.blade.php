73e80ba6eac554e3ce699fe58b15bfba8d2a274c
VojislavD <vojislavdragicevic17@gmail.com>
2022-01-18 19:51:42 +0100

    Display different menu to user and admins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,64 +64,66 @@
                     </a>
                     <!-- end::Menu link -->
 
-                    <p class="text-xs text-gray-600 mt-10 mb-2 px-6 uppercase">Apps</p>
+                    @hasanyrole('admin|super_admin')
+                        <p class="text-xs text-gray-600 mt-10 mb-2 px-6 uppercase">Apps</p>
 
-                    <!-- start::Menu link -->
-                    <a 
-                        x-data="{ linkHover: false }"
-                        @mouseover = "linkHover = true"
-                        @mouseleave = "linkHover = false"
-                        href="{{ route('buildings.index') }}"
-                        class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
-                        :class="{{ Request::segment(1) === 'buildings' }} ? 'bg-black bg-opacity-30' : ''"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" /></svg>
-                        <span 
-                            class="ml-3 transition duration-200" 
-                            :class="linkHover ? 'text-gray-100' : ''"
+                        <!-- start::Menu link -->
+                        <a 
+                            x-data="{ linkHover: false }"
+                            @mouseover = "linkHover = true"
+                            @mouseleave = "linkHover = false"
+                            href="{{ route('buildings.index') }}"
+                            class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
+                            :class="{{ Request::segment(1) === 'buildings' }} ? 'bg-black bg-opacity-30' : ''"
                         >
-                            {{ __('Buildings') }}
-                        </span>
-                    </a>
-                    <!-- end::Menu link -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" /></svg>
+                            <span 
+                                class="ml-3 transition duration-200" 
+                                :class="linkHover ? 'text-gray-100' : ''"
+                            >
+                                {{ __('Buildings') }}
+                            </span>
+                        </a>
+                        <!-- end::Menu link -->
 
-                    <!-- start::Menu link -->
-                    <a 
-                        x-data="{ linkHover: false }"
-                        @mouseover = "linkHover = true"
-                        @mouseleave = "linkHover = false"
-                        href="{{ route('apartments.index') }}"
-                        class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
-                        :class="{{ Request::segment(1) === 'apartments' }} ? 'bg-black bg-opacity-30' : ''"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-                        <span 
-                            class="ml-3 transition duration-200" 
-                            :class="linkHover ? 'text-gray-100' : ''"
+                        <!-- start::Menu link -->
+                        <a 
+                            x-data="{ linkHover: false }"
+                            @mouseover = "linkHover = true"
+                            @mouseleave = "linkHover = false"
+                            href="{{ route('apartments.index') }}"
+                            class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
+                            :class="{{ Request::segment(1) === 'apartments' }} ? 'bg-black bg-opacity-30' : ''"
                         >
-                            {{ __('Apartments') }}
-                        </span>
-                    </a>
-                    <!-- end::Menu link -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                            <span 
+                                class="ml-3 transition duration-200" 
+                                :class="linkHover ? 'text-gray-100' : ''"
+                            >
+                                {{ __('Apartments') }}
+                            </span>
+                        </a>
+                        <!-- end::Menu link -->
 
-                    <!-- start::Menu link -->
-                    <a 
-                        x-data="{ linkHover: false }"
-                        @mouseover = "linkHover = true"
-                        @mouseleave = "linkHover = false"
-                        href="{{ route('tasks.index') }}"
-                        class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
-                        :class="{{ Request::segment(1) === 'tasks' }} ? 'bg-black bg-opacity-30' : ''"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-                        <span 
-                            class="ml-3 transition duration-200" 
-                            :class="linkHover ? 'text-gray-100' : ''"
+                        <!-- start::Menu link -->
+                        <a 
+                            x-data="{ linkHover: false }"
+                            @mouseover = "linkHover = true"
+                            @mouseleave = "linkHover = false"
+                            href="{{ route('tasks.index') }}"
+                            class="flex items-center text-gray-400 px-6 py-3 cursor-pointer hover:bg-black hover:bg-opacity-30 transition duration-200"
+                            :class="{{ Request::segment(1) === 'tasks' }} ? 'bg-black bg-opacity-30' : ''"
                         >
-                            {{ __('Tasks') }}
-                        </span>
-                    </a>
-                    <!-- end::Menu link -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                            <span 
+                                class="ml-3 transition duration-200" 
+                                :class="linkHover ? 'text-gray-100' : ''"
+                            >
+                                {{ __('Tasks') }}
+                            </span>
+                        </a>
+                        <!-- end::Menu link -->
+                    @endrole
                 </nav>
                 <!-- end::Navigation -->
             </aside>
