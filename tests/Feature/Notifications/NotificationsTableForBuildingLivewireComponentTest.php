80a8b93c76e2cced3d66cca17a15e457d2bca1c9
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-15 16:46:08 +0100

    Add tests for notification table for building livewire component

---
+++ tests/Feature/Notifications/NotificationsTableForBuildingLivewireComponentTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Feature\Notifications;
+
+use App\Enums\NotificationStatus;
+use App\Models\Building;
+use App\Models\Notification;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class NotificationsTableForBuildingLivewireComponentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function test_that_component_shows_correct_data()
+    {
+        $building = Building::factory()->create();
+        $notification = Notification::factory()->for($building)->create();
+
+        Livewire::test('notifications.notifications-table-for-building', [
+                'building' => $building
+            ])
+            ->assertSee($notification->subject);
+    }
+
+    /**
+     * @test
+     */
+    public function test_that_component_shows_correct_data_when_status_is_selected()
+    {
+        $building = Building::factory()->create();
+        $notificationScheduled = Notification::factory()->for($building)->scheduled()->create();
+        $notificationProcessing = Notification::factory()->for($building)->processing()->create();
+        $notificationFinished = Notification::factory()->for($building)->finished()->create();
+        $notificationCancelled = Notification::factory()->for($building)->cancelled()->create();
+
+        Livewire::test('notifications.notifications-table-for-building', [
+                'building' => $building
+            ])
+            ->assertSee($notificationScheduled->subject)
+            ->assertSee($notificationProcessing->subject)
+            ->assertSee($notificationFinished->subject)
+            ->assertSee($notificationCancelled->subject);
+
+        Livewire::test('notifications.notifications-table-for-building', [
+                'building' => $building
+            ])
+            ->set('status', NotificationStatus::Scheduled->value)
+            ->assertSee($notificationScheduled->subject)
+            ->assertDontSee($notificationProcessing->subject)
+            ->assertDontSee($notificationFinished->subject)
+            ->assertDontSee($notificationCancelled->subject);
+        
+        Livewire::test('notifications.notifications-table-for-building', [
+                'building' => $building
+            ])
+            ->set('status', NotificationStatus::Processing->value)
+            ->assertSee($notificationProcessing->subject)
+            ->assertDontSee($notificationScheduled->subject)
+            ->assertDontSee($notificationFinished->subject)
+            ->assertDontSee($notificationCancelled->subject);
+
+        Livewire::test('notifications.notifications-table-for-building', [
+                'building' => $building
+            ])
+            ->set('status', NotificationStatus::Finished->value)
+            ->assertSee($notificationFinished->subject)
+            ->assertDontSee($notificationScheduled->subject)
+            ->assertDontSee($notificationProcessing->subject)
+            ->assertDontSee($notificationCancelled->subject);
+
+        Livewire::test('notifications.notifications-table-for-building', [
+                'building' => $building
+            ])
+            ->set('status', NotificationStatus::Cancelled->value)
+            ->assertSee($notificationCancelled->subject)
+            ->assertDontSee($notificationScheduled->subject)
+            ->assertDontSee($notificationProcessing->subject)
+            ->assertDontSee($notificationFinished->subject);
+    }
+}
