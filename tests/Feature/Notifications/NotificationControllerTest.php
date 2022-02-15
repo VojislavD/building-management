70114b69da1d10c8c1ae09af1a93886e469b7c07
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-15 18:21:45 +0100

    Add tests for notification controller

--- tests/Feature/Notifications/NotificationControllerTest.php
+++ tests/Feature/Notifications/NotificationControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Notifications;
 
+use App\Enums\NotificationStatus;
 use App\Models\Building;
+use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -39,5 +41,102 @@ class NotificationControllerTest extends TestCase
             ->assertSeeText(__('Send New Notification'))
             ->assertSee($building->internal_code)
             ->assertSee($building->address);
+    }
+
+    /**
+     * @test
+     */
+    public function test_show_page_can_view_only_authenticated_user()
+    {
+        $notification = Notification::factory()->create();
+
+        $response = $this->get(route('notifications.show', $notification));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * @test
+     */
+    public function test_show_page_show_correct_info()
+    {
+        $notification = Notification::factory()->create();
+
+        $response = $this->actingAs(User::factory()->create())
+            ->get(route('notifications.show', $notification));
+
+        $response->assertOk()
+            ->assertViewIs('notifications.show')
+            ->assertSeeInOrder([
+                $notification->building->internal_code,
+                $notification->building->address,
+                $notification->building->apartments->count(),
+                $notification->subject,
+                $notification->body,
+                $notification->send_at,
+                $notification->created_at,
+                $notification->updated_at,
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_cancel_can_do_only_authenticated_user()
+    {
+        $notification = Notification::factory()->create();
+
+        $response = $this->patch(route('notifications.cancel', $notification));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * @test
+     */
+    public function test_cancel_notification()
+    {
+        $notification = Notification::factory()->create();
+
+        $response = $this->actingAs(User::factory()->create())
+            ->patch(route('notifications.cancel', $notification));
+
+        $response->assertRedirect(route('buildings.show', $notification->building))
+            ->assertSessionHas('notificationCancelled');
+
+        $this->assertDatabaseHas('notifications', [
+            'id' => $notification->id,
+            'status' => NotificationStatus::Cancelled->value
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_delete_can_do_only_authenticated_user()
+    {
+        $notification = Notification::factory()->create();
+
+        $response = $this->delete(route('notifications.delete', $notification));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * @test
+     */
+    public function test_delete_notification()
+    {
+        $notification = Notification::factory()->create();
+
+        $response = $this->actingAs(User::factory()->create())
+            ->delete(route('notifications.delete', $notification));
+
+        $response->assertRedirect(route('buildings.show', $notification->building))
+            ->assertSessionHas('notificationDeleted');
+
+        $this->assertDatabaseMissing('notifications', [
+            $notification->id
+        ]);
     }
 }
