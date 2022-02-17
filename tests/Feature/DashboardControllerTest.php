96840391d04e494033d22d82a25ba3032c09b4a7
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-17 17:07:56 +0100

    Text dashboard controller for admin

--- tests/Feature/DashboardControllerTest.php
+++ tests/Feature/DashboardControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Notification;
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -70,5 +72,40 @@ class DashboardControllerTest extends TestCase
             ->assertSeeText(__('Apartments'))
             ->assertSeeText(__('Tasks'))
             ->assertSeeText(__('Projects'));
+    }
+
+    /**
+     * @test
+     */
+    public function test_dashboard_page_shows_correct_stats()
+    {
+        $admin = User::factory()
+            ->create()
+            ->assignRole('admin');
+
+        $tasksThisMonth = Task::factory(1)->create()->count();
+        $tasksLastMonth = Task::factory(2)->create(['created_at' => now()->subMonth()])->count();
+        $notificationsThisMonth = Notification::factory(3)->create()->count();
+        $notificationsLastMonth = Notification::factory(4)->create(['created_at' => now()->subMonth()])->count();
+
+        $response = $this->actingAs($admin)
+            ->get(route('dashboard'));
+
+        $response->assertOk()
+            ->assertViewIs('dashboard')
+            ->assertSeeInOrder([
+                __('Tasks This Month'),
+                $tasksThisMonth, 
+                __('Tasks Last Month'),
+                $tasksLastMonth,
+                __('Notifications This Month'),
+                $notificationsThisMonth,
+                __('Notifications Last Month'),
+                $notificationsLastMonth
+            ])
+            ->assertSeeText([
+                __('Scheduled Notifications'),
+                __('Pending Tasks')
+            ]);
     }
 }
