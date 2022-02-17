f643b96cf8a15a8d18857fa9e8d87e72e8b63667
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-17 17:18:18 +0100

    Test PendingTasks livewire component

---
+++ tests/Feature/PendingTasksLivewireComponentTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class PendingTasksLivewireComponentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function test_that_component_shows_correct_data()
+    {
+        $task1 = Task::factory()->pending()->create([
+            'created_at' => now()->subDay()
+        ]);
+        Task::factory(4)->pending()->create();
+        $task2 = Task::factory()->pending()->create();
+        $completedTask = Task::factory()->completed()->create();
+        $cancelledTask = Task::factory()->cancelled()->create();
+
+        Livewire::test('tasks.pending-tasks')
+            ->assertSee(__('Pending Tasks'))
+            ->assertSee(str($task2->description)->limit(40))
+            ->assertDontSee([
+                str($task1->description)->limit(40),
+                str($completedTask->description)->limit(40),
+                str($cancelledTask->description)->limit(40),
+            ]);
+    }
+}
