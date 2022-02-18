123f79e4d1a430c0dea10a679107eb8e846dc2d5
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-18 22:44:27 +0100

    test ActiveProject livewire component

---
+++ tests/Feature/Projects/ActiveProjectsLivewireComponentTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Projects;
+
+use App\Models\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ActiveProjectsLivewireComponentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function test_that_component_shows_correct_data()
+    {
+        $pendingProject = Project::factory()->pending()->create();
+        $processingProject = Project::factory()->processing()->create();
+        $finishedProject = Project::factory()->finished()->create();
+        $cancelledProject = Project::factory()->cancelled()->create();
+
+        Livewire::test('projects.active-projects')
+            ->assertSee(__('Active Projects'))
+            ->assertSee([
+                str($pendingProject->name)->limit(50),
+                str($processingProject->name)->limit(50)
+            ])
+            ->assertDontSee([
+                str($finishedProject->name)->limit(50),
+                str($cancelledProject->name)->limit(50)
+            ]);
+    }
+}
