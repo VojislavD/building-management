fabd6a27fa351e9f99e96e0338061ee19135bd26
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-24 09:34:27 +0100

    Uncomment scheduled notification sending command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command(SendBuildingNotifications::class)->everyFiveMinutes();
+        $schedule->command(SendBuildingNotifications::class)->everyFiveMinutes();
     }
 
     /**
