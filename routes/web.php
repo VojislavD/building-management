9d621b6efb1de620f78f2e66c8c06483f41dc8c6
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-24 09:21:06 +0100

    Redirect root to dashboard

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function() {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
